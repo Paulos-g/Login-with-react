52a264b79f12f218aeda51f2c127df6f54c2c724
Paulos Girmacew <paulosgirmachew28@gmail.com>
2026-03-22 22:21:27 +0300

    Add fotter and login check

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import LogIn from "./Login";
 import Input from "./Input";
 
-bool;
+var isLoggedIn = true;
+const date = new Date();
+const currentDate = date.getDate();
+const currentMonth = date.getMonth();
+const currentYear = date.getFullYear();
 
 function App() {
   return (
     <div className="Container">
-      <h1>Hello</h1>
-      <LogIn />
-      <Input text="text" placeholder="username" />
-      <Input text="text" placeholder="password" />
-      <button type="submit">Login</button>
+      {isLoggedIn === true ? (
+        <h1>Hello</h1>
+      ) : (
+        <>
+          <LogIn />
+          <Input text="text" placeholder="username" />
+          <Input text="text" placeholder="password" />
+          <button type="submit">Login</button>
+          <p>
+            {currentDate}/{currentMonth}/{currentYear}G.C
+          </p>
+        </>
+      )}
     </div>
   );
 }
